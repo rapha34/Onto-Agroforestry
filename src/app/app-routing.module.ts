4724ec800b4d2aa0ac984c0d249aa9371db54380
Raphaël CONDE SALAZAR <cayenne34@free.fr>
2020-04-03 17:20:40 +0200

    integration i18n

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { HomeComponent } from './home/home.component';
+import { View2Component } from './view2/view2.component';
+import { View1Component } from './view1/view1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -5,7 +8,16 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [
+    View1Component, View2Component
+  ],
+  imports: [RouterModule.forRoot([
+    { path: 'home', component: HomeComponent },
+    { path: 'view1', component: View1Component },
+    { path: 'view2', component: View2Component },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home', pathMatch: 'full' }
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
